1c91cb132fde589fda2a878b40ff23ade19d9722
saraz <bagas.saras.ft21@mail.umy.ac.id>
2023-12-28 01:11:38 +0700

    Menambah function HomeScreen

--- app/src/main/java/com/example/consumeapi/HomeScreen.kt
+++ app/src/main/java/com/example/consumeapi/HomeScreen.kt
@@ -1,8 +1,25 @@
 package com.example.consumeapi
 
 import DestinasiNavigasi
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.consumeapi.ui.PenyediaViewModel
+import com.example.consumeapi.ui.home.viewmodel.HomeViewModel
 
 object DestinasiHome : DestinasiNavigasi {
     override val route = "home"
     override val titleRes = "kontak"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Int) -> Unit = {},
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+
 }
