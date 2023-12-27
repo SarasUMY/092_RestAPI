7757b45015ece3d13c2c070fa7cb3f16ef08e418
saraz <bagas.saras.ft21@mail.umy.ac.id>
2023-12-28 01:32:23 +0700

    Menambah function updateInsertKontakState

--- app/src/main/java/com/example/consumeapi/ui/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/consumeapi/ui/kontak/viewmodel/InsertViewModel.kt
@@ -8,4 +8,7 @@ import com.example.consumeapi.repository.KontakRepository
 class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel() {
     var insertKontakState by mutableStateOf(InsertUiState())
         private set
+    fun updateInsertKontakState(insertUiEvent: InsertUiEvent) {
+        insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
 }
