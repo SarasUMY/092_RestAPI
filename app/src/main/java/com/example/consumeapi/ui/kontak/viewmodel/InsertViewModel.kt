ea42c851b539138f3077480c3af10df931580487
saraz <bagas.saras.ft21@mail.umy.ac.id>
2023-12-28 01:34:19 +0700

    Menambah function insertKontak

--- app/src/main/java/com/example/consumeapi/ui/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/consumeapi/ui/kontak/viewmodel/InsertViewModel.kt
@@ -3,12 +3,24 @@ package com.example.consumeapi.ui.kontak.viewmodel
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.consumeapi.repository.KontakRepository
+import kotlinx.coroutines.launch
 
 class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel() {
     var insertKontakState by mutableStateOf(InsertUiState())
         private set
     fun updateInsertKontakState(insertUiEvent: InsertUiEvent) {
         insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertKontak() {
+        viewModelScope.launch {
+            try {
+                kontakRepository.insertKontak(insertKontakState.insertUiEvent.toKontak())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
     }
 }
