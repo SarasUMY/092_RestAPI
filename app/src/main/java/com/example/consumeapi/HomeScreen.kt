43d7ac9994a15fc8c30aacbb76f5365a5e7b7e16
saraz <bagas.saras.ft21@mail.umy.ac.id>
2023-12-28 01:20:28 +0700

    Menambah FloatingActionButton didalam Scaffold

--- app/src/main/java/com/example/consumeapi/HomeScreen.kt
+++ app/src/main/java/com/example/consumeapi/HomeScreen.kt
@@ -1,12 +1,19 @@
 package com.example.consumeapi
 
 import DestinasiNavigasi
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.consumeapi.ui.PenyediaViewModel
 import com.example.consumeapi.ui.home.viewmodel.HomeViewModel
@@ -34,6 +41,18 @@ fun HomeScreen(
                 canNavigateBack = false,
                 scrollBehavior = scrollBehavior,
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add Kontak"
+                )
+            }
         }
     ) {
 
