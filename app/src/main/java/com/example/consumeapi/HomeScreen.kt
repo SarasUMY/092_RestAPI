de42b68983ac9851f000fd64cc6c45aa976c6543
saraz <bagas.saras.ft21@mail.umy.ac.id>
2023-12-28 01:16:14 +0700

    Menambah Scaffold TopAppBarKontak pada function HomeScreen

--- app/src/main/java/com/example/consumeapi/HomeScreen.kt
+++ app/src/main/java/com/example/consumeapi/HomeScreen.kt
@@ -2,8 +2,11 @@ package com.example.consumeapi
 
 import DestinasiNavigasi
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.consumeapi.ui.PenyediaViewModel
 import com.example.consumeapi.ui.home.viewmodel.HomeViewModel
@@ -21,5 +24,18 @@ fun HomeScreen(
     onDetailClick: (Int) -> Unit = {},
     viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBarKontak(
+                title = DestinasiHome.titleRes,
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior,
+            )
+        }
+    ) {
+
+    }
 }
